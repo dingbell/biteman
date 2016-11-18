ea79fd61f27307bbd5c7d12852807d7218dc4afe
Xiaoyu Chen <smpatwcxy@gmail.com>
2016-11-18 02:30:39 +0000

    add

---
+++ public/vmenu.php
@@ -0,0 +1,54 @@
+<?php
+    // Implementation of viewing the menu control
+
+    // configuration
+    require("../includes/config.php"); 
+
+    // if user reached page via GET (as by clicking a link or via redirect)
+    if ($_SERVER["REQUEST_METHOD"] == "GET")
+    {
+        // else render form
+        render("vmenu_form.php", ["title" => "Vmenu"]);
+    }
+
+    // else if user reached page via POST (as by submitting a form via POST)
+    else if ($_SERVER["REQUEST_METHOD"] == "POST")
+    {
+        // validate submission
+        if (empty($_POST["symbol"]))
+        {
+            apologize("You must provide the symbol.");
+        }
+        if (preg_match("/^\d+$/", $_POST["shares"])!= TRUE)
+        {
+            apologize("You must provide the a vailid whole shareds of stocks.");
+        }
+        else
+        {
+            $stock = lookup($_POST["symbol"]);
+            $price = $stock["price"];
+            if($stock === false)
+            {
+                apologize("Please provide a valid symbol.");
+            }
+            else
+            {
+                $cash = CS50::query("SELECT cash from users where id = ?", $_SESSION["id"]);
+                $debt = $price * $_POST["shares"];
+                if ($debt > $cash[0]["cash"])
+                {
+                    apologize("You don't have enough cash.");
+                }
+                else
+                {
+                    CS50::query("INSERT INTO portfolios (user_id, symbol, shares) VALUES(?, upper(?), ?) ON DUPLICATE KEY UPDATE shares = shares + ?",$_SESSION["id"],$stock["symbol"],$_POST["shares"],$_POST["shares"]);
+                    CS50::query("UPDATE users SET cash = cash - ? WHERE id = ?", $debt, $_SESSION["id"]);
+                    CS50::query("INSERT INTO transaction (user_id, transaction, time, symbol, shares, price) VALUES(?,?,now(),?,?,?)", $_SESSION["id"], "BUY", $stock["symbol"],$_POST["shares"],$stock["price"]);
+                    redirect("/");
+                }
+            }
+        }
+        
+    }
+
+?>
---
+++ views/vmenu_form.php
@@ -0,0 +1,29 @@
+<form action="vmenu.php" method="post">
+    <fieldset>
+        <div>
+            <?php
+                $rows= cs50::query("SELECT name,price,picture FROM food WHERE restid = ?", $_SESSION["id"]);
+                echo "<table style='width:100%'>
+                    <tr>
+                    <th>name</th>
+                    <th>price</th>
+                    <th>picture</th>
+                    </tr>";
+                    foreach ($rows as $row):
+                        echo "<tr>";
+                        echo "<td>" . $row['name'] . "</td>";
+                        echo "<td>" . $row['price'] . "</td>";
+                        echo "<td>" . "<img src= ".$row["picture"]." height='50' >". "</td>";
+   /*                     echo " <td><select name='cars'>
+                              <option value='1'>1</option>
+                                <option value='2'>2</option>
+                                <option value='3'>3</option>
+                                <option value='4'>4</option>
+                                </select></td>";
+    */                   echo "</tr>";
+                    endforeach;
+                echo"</table>"
+            ?>
+        </div>
+    </fieldset>
+</form>
