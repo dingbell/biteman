b4568dd9e5b7da02bc56bb839437a95f43c84cc8
Chang Cui <ascuichang@163.com>
2016-11-20 22:23:09 +0000

    fix javascript error

--- views/search_rest.php
+++ views/search_rest.php
@@ -1,3 +1,11 @@
+<style>
+      #map {
+        height: 400px;
+        width: 100%;
+       }
+</style>
+
+
 <p>
     <table class = "table table-stripped">
     <thead>
@@ -8,6 +16,8 @@
             <th>Region</th>
             <th>Province</th>
             <th>Postcode</th>
+            <th>Province</th>
+            <th>Postcode</th>
         </tr>
     </thead>
     <tbody>
@@ -16,17 +26,42 @@
     <?php foreach ($positions as $position): ?>
     
         <tr>
+            
             <td><?= $position["Name"] ?></td>
             <td><?= $position["Add1"] ?></td>
             <td><?= $position["Add2"] ?></td>
             <td><?= $position["Region"] ?></td>
             <td><?= $position["Prov"] ?></td>
             <td><?= $position["Postcode"] ?></td>
+            <td><?= $position["latitude"] ?></td>
+            <td><?= $position["longitude"] ?></td>
         </tr>
-    
+        
+        <?= $latitude = $position["latitude"];?>
+        <?= $longitude = $position["longitude"];?>
+        
     <?php endforeach ?>
+    
     </tbody>
-</table>
-
+    </table>
 </p>
 
+    <h3>Google Map</h3>
+    <div id="map"></div>
+    <script>
+        function initMap() {
+            var uluru = {lat: parseFloat('<?php echo "$latitude";?>') , lng: parseFloat('<?php echo "$longitude";?>')};
+            var map = new google.maps.Map(document.getElementById('map'), {
+                zoom: 16,
+                center: uluru
+                //center: {lat:43.472729, lng:-80.542165}
+            });
+            var marker = new google.maps.Marker({
+                position: uluru,
+                map: map
+            });
+        }
+    </script>
+    <script async defer
+        src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBtPDrH4urnvLWbPv0kWSO0uzyijxcahS4&callback=initMap">
+    </script>
